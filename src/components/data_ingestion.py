814dfdd2145284858d0f910d2b65006f3d2bf997
ramsec3t10-dev <rams.ec3t10@gct.ac.in>
2025-12-24 22:54:03 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -54,7 +56,10 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     dataTransformation = DataTransformation()
-    dataTransformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = dataTransformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
     
 
